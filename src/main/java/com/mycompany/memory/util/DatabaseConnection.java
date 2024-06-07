e16503c7952cb2a07f5927907d573a31edcf0a70
ieddy1992 <128192357+ieddy1992@users.noreply.github.com>
2024-06-07 01:24:57 -0300

    Update DatabaseConnection.java

--- src/main/java/com/mycompany/memory/util/DatabaseConnection.java
+++ src/main/java/com/mycompany/memory/util/DatabaseConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://memory-memory.g.aivencloud.com:27047/memory";
-    private static final String USER = "27047";
+    private static final String URL = "jdbc:postgresql://memory-memory.g.aivencloud.com:27047/defaultdb?sslmode=require";
+    private static final String USER = "avnadmin";
     private static final String PASSWORD = "AVNS_WT1RatM5TkttzoYIHd5";
 
     public static Connection getConnection() throws SQLException {
